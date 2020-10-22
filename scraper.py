bfc41b3ac7d7af19c6623a59e3b988f1237a9d84
gabe7murphy <gabe7murphy@gmail.com>
2020-10-21 22:20:43 -0400

    function error

--- scraper.py
+++ scraper.py
@@ -5,43 +5,44 @@ from newAnalyzer import SentimentIntensityAnalyzer
 import time
 import pprint
 
+def my_scraper():
+    date_sentiments = {}
+    unique_dates = set()
+    for i in range(1,30):
+        page = urlopen('https://www.businesstimes.com.sg/search/state%20street?page='+str(i)).read()
+        soup = BeautifulSoup(page, features="html.parser")
+        posts = soup.findAll("div", {"class": "media-body"})
+        for post in posts:
+    #        post = posts[0]
+            time.sleep(0.1)
+            url = post.a['href']
+            date = post.time.text
+            unique_dates.add(date)
+            print(date, url)
+            try:
+                link_page = urlopen(url).read()
+            except:
+                url = url[:-2]
+                link_page = urlopen(url).read()
+            link_soup = BeautifulSoup(link_page)
+            sentences = link_soup.findAll("p")
+            passage = ""
+            for sentence in sentences:
+                passage += sentence.text
+            sia = SentimentIntensityAnalyzer()
+            sentiment = sia.polarity_scores(passage)['compound']
+            date_sentiments.setdefault(date, []).append(sentiment)
 
-date_sentiments = {}
-unique_dates = set()
-for i in range(1,30):
-    page = urlopen('https://www.businesstimes.com.sg/search/state%20street?page='+str(i)).read()
-    soup = BeautifulSoup(page, features="html.parser")
-    posts = soup.findAll("div", {"class": "media-body"})
-    for post in posts:
-#        post = posts[0]
-        time.sleep(0.1)
-        url = post.a['href']
-        date = post.time.text
-        unique_dates.add(date)
-        print(date, url)
-        try:
-            link_page = urlopen(url).read()
-        except:
-            url = url[:-2]
-            link_page = urlopen(url).read()
-        link_soup = BeautifulSoup(link_page)
-        sentences = link_soup.findAll("p")
-        passage = ""
-        for sentence in sentences:
-            passage += sentence.text
-        sia = SentimentIntensityAnalyzer()
-        sentiment = sia.polarity_scores(passage)['compound']
-        date_sentiments.setdefault(date, []).append(sentiment)
+    date_sentiment = {}
 
-date_sentiment = {}
+    for k,v in date_sentiments.items():
+        date_sentiment[datetime.strptime(k, '%d %b %Y').date() + timedelta(days=1)] = round(sum(v)/float(len(v)),3)
 
-for k,v in date_sentiments.items():
-    date_sentiment[datetime.strptime(k, '%d %b %Y').date() + timedelta(days=1)] = round(sum(v)/float(len(v)),3)
+    earliest_date = min(date_sentiment.keys())
 
-earliest_date = min(date_sentiment.keys())
+    print(earliest_date)
+    print(date_sentiment)
 
-def my_scraper
+    return (date_sentiment, earliest_date)
 
-print(earliest_date)
-
-print(date_sentiment)
+    
--- trader.py
+++ trader.py
@@ -1,14 +1,13 @@
 from __future__ import (absolute_import, division, print_function,
                             unicode_literals)
 
-from scraper import *
 from datetime import datetime, timedelta
+from scraper import my_scraper
 import warnings
 warnings.filterwarnings('ignore')
 
 import backtrader as bt
 import backtrader.indicators as btind
-
 import pandas as pd
 import os.path
 import time
